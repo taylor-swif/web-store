8e518306bb853b126737cf7f862c948fd6460433
Krzysztof Ligarski <kufa003@gmail.com>
2023-12-29 13:50:38 +0100

    Added WineTaste and WineColor API endpoints

--- server/base/api/urls.py
+++ server/base/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.conf.urls import include
-from .views import MyTokenObtainPairView, WineViewSet, CountryViewSet
+from .views import MyTokenObtainPairView, WineViewSet, CountryViewSet, WineTasteViewSet, WineColorViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'wines', WineViewSet)
 router.register(r'countries', CountryViewSet)
+router.register(r'wine_tastes', WineTasteViewSet)
+router.register(r'wine_colors', WineColorViewSet)
 
 urlpatterns = [
     # path('profile/', get_profile),
--- server/base/api/views.py
+++ server/base/api/views.py
@@ -1,6 +1,6 @@
-from .serializers import MyTokenObtainPairSerializer, WineSerializer, CountrySerializer
+from .serializers import MyTokenObtainPairSerializer, WineSerializer, CountrySerializer, WineColorSerializer, WineTasteSerializer
 from .permissions import IsManagerOrReadOnly
-from base.models import Wine, Country
+from base.models import Wine, Country, WineColor, WineTaste
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -18,4 +18,14 @@ class WineViewSet(viewsets.ModelViewSet):
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    permission_classes = (IsManagerOrReadOnly, )
+
+class WineColorViewSet(viewsets.ModelViewSet):
+    queryset = WineColor.objects.all()
+    serializer_class = WineColorSerializer
+    permission_classes = (IsManagerOrReadOnly, )
+
+class WineTasteViewSet(viewsets.ModelViewSet):
+    queryset = WineTaste.objects.all()
+    serializer_class = WineTasteSerializer
     permission_classes = (IsManagerOrReadOnly, )
